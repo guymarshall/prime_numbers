84ddac06652072a54120953c392da2ae224d02a5
Guy Marshall <guymarshall9@gmail.com>
2023-06-14 22:08:35 +0100

    Swap is_prime_number if statements with branch-less code

--- src/math.rs
+++ src/math.rs
@@ -1,15 +1,21 @@
 #![forbid(unsafe_code)]
 
 pub fn is_prime_number(number: i32) -> bool {
-    if number % 2 == 0 {
-        return false;
+    let is_even = (number & 1) == 0;
+    let is_divisible = (number % 3) == 0;
+
+    let is_not_prime = is_even || is_divisible;
+
+    let mut counter = 5;
+    let mut increment = 2;
+    let mut is_prime = true;
+
+    while counter * counter <= number {
+        let is_divisible = (number % counter) == 0;
+        is_prime &= !is_divisible;
+        increment = 6 - increment;
+        counter += increment;
     }
-    let mut counter: i32 = 3;
-    while counter < number {
-        if number % counter == 0 {
-            return false;
-        }
-        counter += 2;
-    }
-    return true;
+
+    return number > 1 && is_prime && !is_not_prime;
 }
