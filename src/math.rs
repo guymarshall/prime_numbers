5a954a2b41f8bf46b7c3c720d974f4e9cf931ada
Guy Marshall <guymarshall9@gmail.com>
2023-06-14 22:10:12 +0100

    Add type annotations to is_prime_number()

--- src/math.rs
+++ src/math.rs
@@ -1,17 +1,17 @@
 #![forbid(unsafe_code)]
 
 pub fn is_prime_number(number: i32) -> bool {
-    let is_even = (number & 1) == 0;
-    let is_divisible = (number % 3) == 0;
+    let is_even: bool = (number & 1) == 0;
+    let is_divisible: bool = (number % 3) == 0;
 
-    let is_not_prime = is_even || is_divisible;
+    let is_not_prime: bool = is_even || is_divisible;
 
-    let mut counter = 5;
-    let mut increment = 2;
-    let mut is_prime = true;
+    let mut counter: i32 = 5;
+    let mut increment: i32 = 2;
+    let mut is_prime: bool = true;
 
     while counter * counter <= number {
-        let is_divisible = (number % counter) == 0;
+        let is_divisible: bool = (number % counter) == 0;
         is_prime &= !is_divisible;
         increment = 6 - increment;
         counter += increment;
